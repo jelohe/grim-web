58adc7a18c2b4c4a6e3782dc947711adb63e3e38
jelohe <jsus.90@gmail.com>
2026-01-16 18:43:21 +0100

    add tests to scrolls liveView

--- test/grim_web/live/user_live/scroll_test.exs
+++ test/grim_web/live/user_live/scroll_test.exs
@@ -11,10 +11,58 @@ defmodule GrimWeb.UserLive.ScrollTest do
   end
 
   describe "scrolls page" do
-    test "renders create button", %{conn: conn} do
+    test "renders a create button", %{conn: conn} do
       {:ok, _lv, html} = live(conn, ~p"/scrolls")
 
       assert html =~ "o open e lipu sin"
+    end
+
+    test "creates a new scroll", %{conn: conn} do
+      {:ok, lv, _html} = live(conn, ~p"/scrolls")
+
+      lv
+      |> form("#scroll-editor", scroll: %{name: "my note", content: "hello"})
+      |> render_submit()
+
+      assert has_element?(lv, "li", "my note")
+    end
+
+    test "updates an existing scroll", %{conn: conn, user: user} do
+      %Grim.Scroll{user_id: user.id, name: "old scroll", content: "old text" }
+        |> Grim.Repo.insert!()
+
+      {:ok, lv, _html} = live(conn, ~p"/scrolls")
+
+      lv
+      |> element("li", "old scroll")
+      |> render_click()
+
+      assert has_element?(lv, "li", "old scroll")
+
+      lv
+      |> form("#scroll-editor", scroll: %{name: "new scroll", content: "new text"})
+      |> render_submit()
+
+      assert has_element?(lv, "li", "new scroll")
+      refute has_element?(lv, "li", "old scroll")
+    end
+
+    test "clicking a scroll loads it into the editor", %{conn: conn, user: user} do
+      %Grim.Scroll{user_id: user.id, name: "existing scroll", content: "text content of the existing scroll" }
+      |> Grim.Repo.insert!()
+
+      {:ok, lv, _html} = live(conn, ~p"/scrolls")
+
+      lv
+      |> element("li", "existing scroll")
+      |> render_click()
+
+      assert has_element?(lv, "input[value=\"existing scroll\"]")
+      assert has_element?(
+        lv,
+        "textarea",
+        "text content of the existing scroll"
+      )
     end
   end
 end
